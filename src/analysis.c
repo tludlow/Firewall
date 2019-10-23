65e4af5d8d6f2e7f0ab59a3633945321966c17e7
Thomas Ludlow <Thomas.Ludlow@warwick.ac.uk>
2019-10-23 21:02:31 +0100

    finished in the lab with research and some minor additions to start packet analysing

--- src/analysis.c
+++ src/analysis.c
@@ -5,6 +5,23 @@
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 
+
+//Ethernet packet header
+struct eth_header {
+  u_char  destination_mac[6], source_mac[6]; //from, to mac addresses
+  u_short type;
+};
+
+
+void intercept_ethernet_packet() {
+    //Want to do a basic sanity check - is the packet header of the required length? (ethernet header length is 14 bytes)
+    if() {
+
+    }
+
+}
+
 void analyse(struct pcap_pkthdr *header, const unsigned char *packet, int verbose) {
-  // TODO your part 2 code here
+    //struct ether_header * eth_header = (struct ether_header *) data;
+    //printf("\nType: %hu\n", eth_header->ether_type);
 }
